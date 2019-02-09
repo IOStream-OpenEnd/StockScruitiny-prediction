0c59100c51e4b5cf3a196693be43c56d00fa8c8f
vipul chhabra <vipulchhabra555@gmail.com>
2019-02-09 21:14:41 +0530

    Linear and Polynomial Regression

---
+++ predict1.py
@@ -0,0 +1,66 @@
+# Polynomial Regression
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+
+# Importing the dataset
+dataset = pd.read_csv('data.csv')
+X = dataset.iloc[:, 0:1].values
+y = dataset.iloc[:, 1].values
+
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Feature Scaling
+"""from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)"""
+
+# Fitting Linear Regression to the dataset
+lin_reg = LinearRegression()
+lin_reg.fit(X, y)
+
+# Fitting Polynomial Regression to the dataset
+poly_reg = PolynomialFeatures(degree = 4)
+X_poly = poly_reg.fit_transform(X)
+poly_reg.fit(X_poly, y)
+lin_reg_2 = LinearRegression()
+lin_reg_2.fit(X_poly, y)
+
+# Visualising the Linear Regression results
+plt.scatter(X, y, color = 'red')
+plt.plot(X, lin_reg.predict(X), color = 'blue')
+plt.title('Linear Regression')
+plt.xlabel('Time')
+plt.ylabel('High')
+plt.show()
+
+# Visualising the Polynomial Regression results
+plt.scatter(X, y, color = 'red')
+plt.plot(X, lin_reg_2.predict(poly_reg.fit_transform(X)), color = 'blue')
+plt.title('Polynomial Regression')
+plt.xlabel('Time')
+plt.ylabel('High')
+plt.show()
+
+# Visualising the Polynomial Regression results (for higher resolution and smoother curve)
+X_grid = np.arange(min(X), max(X), 0.1)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, y, color = 'red')
+plt.plot(X_grid, lin_reg_2.predict(poly_reg.fit_transform(X_grid)), color = 'blue')
+plt.title('(Polynomial Regression)')
+plt.xlabel('Time')
+plt.ylabel('High')
+plt.show()
+
+# Predicting a new result with Linear Regression
+print(lin_reg.predict([[15.42]]))
+
+# Predicting a new result with Polynomial Regression
+print(lin_reg_2.predict(poly_reg.fit_transform([[15.42]])))
